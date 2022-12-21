03dc0100838b4fed0baa1eb1954538cced5f09b3
Lars Salembier <lars.salembier@gmail.com>
2022-12-22 00:46:06 +0100

    Made things less cluttered to start of.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
-const Koa = require("koa");
-const config = require("config");
-const { getLogger } = require("./core/logging");
+const Koa = require('koa');
+const config = require('config');
 
-const NODE_ENV = config.get("env");
-const LOG_LEVEL = config.get("logLevel");
-const LOG_DISABLED = config.get("logDisabled");
+const { getLogger } = require('./core/logging');
+
+const NODE_ENV = config.get('env');
+const LOG_LEVEL = config.get('logLevel');
+const LOG_DISABLED = config.get('logDisabled');
 
 console.log(`log level ${LOG_LEVEL}, logs enabled: ${!LOG_DISABLED}`);
 
@@ -12,9 +13,9 @@ const app = new Koa();
 const logger = getLogger();
 
 app.use(async (ctx, next) => {
-  ctx.body = "Hello world";
+  ctx.body = 'Hello world';
   next();
 });
 
-logger.info(`🚀 Server listening on http://localhost:9000`);
+logger.info('🚀 Server listening on http://localhost:9000');
 app.listen(9000);
