b9a4ebaa7a4118b5362c191513535446fed5b6cc
larssalembier <lars.salembier@gmail.com>
2022-12-21 14:13:12 +0100

    Moved logging.

--- src/index.js
+++ src/index.js
@@ -1,13 +1,8 @@
 const Koa = require("koa");
-const winston = require("winston");
+const { getLogger } = require("./core/logging");
 
 const app = new Koa();
-
-const logger = winston.createLogger({
-  level: "info",
-  format: winston.format.simple(),
-  transports: [new winston.transports.Console({ silent: false })],
-});
+const logger = getLogger();
 
 app.use(async (ctx, next) => {
   ctx.body = "Hello world";
