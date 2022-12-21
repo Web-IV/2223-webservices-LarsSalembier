8c391f462e0fd795ab5675578b802be9b1a14d2b
larssalembier <lars.salembier@gmail.com>
2022-12-21 14:10:31 +0100

    Added logging.

--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
 const Koa = require("koa");
+const winston = require("winston");
+
 const app = new Koa();
 
-app.use(async (ctx, next) => {
-  console.log(ctx);
-  next();
+const logger = winston.createLogger({
+  level: "info",
+  format: winston.format.simple(),
+  transports: [new winston.transports.Console({ silent: false })],
 });
+
 app.use(async (ctx, next) => {
   ctx.body = "Hello world";
   next();
 });
-app.use(async (ctx, next) => {
-  console.log(ctx);
-  next();
-});
 
+logger.info(`🚀 Server listening on http://localhost:9000`);
 app.listen(9000);
