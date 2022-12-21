ad8fb014071c3a8e64ab62e15082ec2450f3fd32
Lars Salembier <lars.salembier@gmail.com>
2022-12-22 00:59:49 +0100

    Added body parser

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const Koa = require('koa');
 const config = require('config');
 const { getLogger } = require('./core/logging');
+const bodyParser = require('koa-bodyparser');
 
 const NODE_ENV = config.get('env');
 const LOG_LEVEL = config.get('logLevel');
@@ -11,8 +12,16 @@ console.log(`log level ${LOG_LEVEL}, logs enabled: ${!LOG_DISABLED}`);
 const app = new Koa();
 const logger = getLogger();
 
+app.use(bodyParser());
+
 app.use(async (ctx, next) => {
-  ctx.body = 'Hello world';
+  logger.info(JSON.stringify(ctx.request));
+  logger.info(JSON.stringify(ctx.request.body));
+  if (ctx.request.method === 'GET' && ctx.request.url === "/api/transactions") {
+    ctx.body = "[{'user': 'Benjamin', 'amount': 100, 'place': 'Irish Pub', date: '2021-08-15' }]";
+  } else {
+    ctx.body = 'Goodbye world';
+  }
   next();
 });
 
