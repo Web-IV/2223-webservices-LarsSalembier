e76ee94894cee9f206e5292378f2fe9ac4ff9545
Lars Salembier <lars.salembier@gmail.com>
2022-12-22 02:46:14 +0100

    Added CORS

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 const Koa = require('koa');
 const config = require('config');
-const { initializeLogger, getLogger } = require('./core/logging');
 const bodyParser = require('koa-bodyparser');
+const koaCors = require('@koa/cors');
+const { initializeLogger, getLogger } = require('./core/logging');
 const installRest = require('./rest');
 
 const NODE_ENV = config.get('env');
+const CORS_ORIGINS = config.get('cors.origins');
+const CORS_MAX_AGE = config.get('cors.maxAge');
 const LOG_LEVEL = config.get('logLevel');
 const LOG_DISABLED = config.get('logDisabled');
 
@@ -15,6 +18,20 @@ initializeLogger({
 });
 
 const app = new Koa();
+
+// Add CORS
+app.use(
+  koaCors({
+    origin: (ctx) => {
+      if (CORS_ORIGINS.includes(ctx.request.header.origin)) {
+        return ctx.request.header.origin;
+      }
+      return CORS_ORIGINS[0];
+    },
+    allowHeaders: ['Accept', 'Content-Type', 'Authorization'],
+    maxAge: CORS_MAX_AGE,
+  })
+);
 
 const logger = getLogger();
 
