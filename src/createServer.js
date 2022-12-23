7adb6d803be0c8404c0e29ec528b1fe3cce1c560
larssalembier <lars.salembier@gmail.com>
2022-12-23 04:02:42 +0100

    Add request logs

--- src/createServer.js
+++ src/createServer.js
@@ -41,6 +41,31 @@ module.exports = async function createServer() {
 
   app.use(bodyParser());
 
+  app.use(async (ctx, next) => {
+    const logger = getLogger();
+    logger.info(`${emoji.get("fast_forward")} ${ctx.method} ${ctx.url}`);
+
+    const getStatusEmoji = () => {
+      if (ctx.status >= 500) return emoji.get("skull");
+      if (ctx.status >= 400) return emoji.get("x");
+      if (ctx.status >= 300) return emoji.get("rocket");
+      if (ctx.status >= 200) return emoji.get("white_check_mark");
+      return emoji.get("rewind");
+    };
+
+    try {
+      await next();
+
+      logger.info(`${getStatusEmoji()} ${ctx.method} ${ctx.status} ${ctx.url}`);
+    } catch (error) {
+      logger.error(`${emoji.get("x")} ${ctx.method} ${ctx.status} ${ctx.url}`, {
+        error,
+      });
+
+      throw error;
+    }
+  });
+
   installRest(app);
 
   return {
